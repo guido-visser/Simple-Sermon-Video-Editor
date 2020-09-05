eea5ea12ec61a300dc7b1eb1d43efaa11ef97ffa
Guido Visser <guido.vsr@gmail.com>
2020-09-05 20:24:45 +0200

    🎨 Added antd; 🚀 Added more testing stuff; ⚙ Added craco

--- server.js
+++ server.js
@@ -3,32 +3,65 @@ const bodyParser = require("body-parser");
 const ffmpeg = require("fluent-ffmpeg");
 const path = require("path");
 const app = express();
+const _ = require("lodash");
 var longpoll = require("express-longpoll")(app);
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 longpoll.create("/poll");
+
+let currentJob = null;
 
 app.get("/ping", function (req, res) {
     return res.json({ ping: "pong" });
 });
 
-app.get("/merge", function (req, res) {
-    res.json({ msg: "Merge started" });
-    ffmpeg("./files/00000.mts")
+app.post("/merge", function (req, res) {
+    //res.json({ msg: "Merge started" });
+    const { socket, ...other } = res;
+
+    const files = req.body;
+
+    let index = 0;
+    let ff = null;
+    try {
+        _.forOwn(files, (filePath) => {
+            if (index === 0) {
+                ff = ffmpeg(filePath).withVideoCodec("libx264").addInputOption(["-hwaccel dxva2"]);
+            } else {
+                ff.input(filePath);
+            }
+            index++;
+        });
+
+        ff.on("end", function () {
+            longpoll.publish("/poll", { action: "renderUpdate", msg: "files have been merged succesfully" });
+            console.log("files have been merged succesfully");
+        })
+            .on("error", function (err) {
+                longpoll.publish("/poll", { action: "renderUpdate", error: "an error happened: " + err.message });
+                console.log("an error happened: " + err.message);
+            })
+            .on("progress", function (info) {
+                longpoll.publish("/poll", { action: "renderUpdate", ...info });
+            })
+            .mergeToFile("./files/mergedService.mkv");
+        res.json({ success: "yeah" });
+    } catch (e) {
+        res.json({ error: e });
+    }
+
+    /* currentJob = ffmpeg("./files/00000.mts")
+        .withVideoCodec("libx264")
+        .addInputOption(["-hwaccel dxva2"])
         .input("./files/00001.mts")
         .on("progress", function (info) {
             longpoll.publish("/poll", { action: "renderUpdate", ...info });
         })
-        .on("end", function () {
-            longpoll.publish("/poll", { action: "renderUpdate", msg: "files have been merged succesfully" });
-            console.log("files have been merged succesfully");
-        })
-        .on("error", function (err) {
-            longpoll.publish("/poll", { action: "renderUpdate", error: "an error happened: " + err.message });
-            console.log("an error happened: " + err.message);
-        })
-        .mergeToFile("./files/mergedService.mkv")
-        .videoCodec("vp9");
+        
+        .mergeToFile("./files/mergedService.mkv"); */
 });
 
 app.get("/test", (req, res) => {
@@ -39,9 +72,21 @@ app.get("/test", (req, res) => {
 
     ffmpeg.getAvailableEncoders(function (err, encoders) {
         console.log("Available encoders:");
-        res.json({ encoders });
+        let video_encoders = {};
+        _.forOwn(encoders, (encoder, key) => {
+            if (encoder.type === "video") {
+                video_encoders[key] = encoder;
+            }
+        });
+        res.json({
+            encoders: video_encoders,
+        });
         console.dir(encoders);
     });
+});
+
+app.get("/stop", function (req, res) {
+    res.json({ currentJob });
 });
 
 app.get("/", function (req, res) {
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.scss";
 import wretch from "wretch";
+import { Input } from "antd";
 
 class App extends React.PureComponent<any, any> {
-    state = { timemark: "00:00:00:00", fps: 0 };
+    state = { timemark: "00:00:00:00", fps: 0, file1: "", file2: "", file3: "" };
     componentDidMount() {
         this.poll();
     }
@@ -24,8 +25,9 @@ class App extends React.PureComponent<any, any> {
     };
 
     handleClick = () => {
+        const { file1, file2, file3 } = this.state;
         wretch("/merge")
-            .get()
+            .post({ file1, file2 })
             .json((res) => console.log(res));
     };
 
@@ -35,14 +37,30 @@ class App extends React.PureComponent<any, any> {
             .json((res) => console.log(res));
     };
 
+    handleStop = () => {
+        wretch("/stop")
+            .get()
+            .json((res) => console.log(res));
+    };
+
+    handleFileInputChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ [name]: event.target.value });
+    };
+
     render() {
-        const { timemark, fps } = this.state;
+        const { timemark, fps, file1, file2, file3 } = this.state;
         return (
             <div className="App">
                 <button onClick={this.handleClick}>Merge files</button>
                 <button onClick={this.handleTestClick}>Test</button>
+                <button onClick={this.handleStop}>Stop</button>
                 <div>Timemark: {timemark}</div>
                 <div>FPS: {fps}</div>
+                <div>
+                    <Input value={file1} onChange={this.handleFileInputChange("file1")} placeholder="Video file 1" />
+                    <Input value={file2} onChange={this.handleFileInputChange("file2")} placeholder="Video file 2" />
+                    <Input value={file3} onChange={this.handleFileInputChange("file3")} placeholder="Video file 3" />
+                </div>
             </div>
         );
     }
