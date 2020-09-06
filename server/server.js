6f62ff4a8ebbd0bebb87292ebcbb8ffc39713184
Guido Visser <guido.vsr@gmail.com>
2020-09-06 20:53:36 +0200

    🧽 Cleanup

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const lp = require("./longpoll");
 
 lp.create("/poll");
 
-let currentJob = null;
-
 app.get("/ping", function (req, res) {
     return res.json({ ping: "pong" });
 });
@@ -14,10 +12,6 @@ const mainRoutes = require("./routes/main");
 const processRoutes = require("./routes/process");
 app.use("/api/main", mainRoutes);
 app.use("/api/process", processRoutes);
-
-app.get("/stop", function (req, res) {
-    res.json({ currentJob });
-});
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
